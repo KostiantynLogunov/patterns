3f8f87b2a50a229c3d2f65045b52c832328f471a
KostiantynLogunov <777koc@gmail.com>
2020-02-16 14:31:56 +0200

    Singleton completed

---
+++ Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton = Singleton.GetInstance("Привет, мир!");
+            var singleton2 = Singleton.GetInstance("Здравствуй, космос!");
+            Console.WriteLine(singleton2.Data);
+        }
+    }
+}
