144731bf42f455adbaa0f7000aae19d8c7b6d7b3
KostiantynLogunov <777koc@gmail.com>
2020-02-29 18:39:18 +0200

    builder completed

---
+++ Builder/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace Builder
+{
+    class Client
+    {
+        void Main()
+        {
+            Builder builder = new ConcreteBuilder();
+            Director director = new Director(builder);
+            director.Construct();
+            Product product = builder.GetResult();
+        }
+    }
+}
