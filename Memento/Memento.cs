84ab7117e80aad58762b04ceb92dab95d17959e6
KostiantynLogunov <777koc@gmail.com>
2020-03-05 18:02:01 +0200

    memento was completed

---
+++ Memento/Memento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento
+{
+    /*
+     Memento: хранитель, который сохраняет состояние объекта Originator и 
+     предоставляет полный доступ только этому объекту Originator
+         */
+    class Memento
+    {
+        public string State { get; private set; }
+        public Memento(string state)
+        {
+            this.State = state;
+        }
+    }
+}
---
+++ Memento/Originator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento
+{
+    // Originator: создает объект хранителя для сохранения своего состояния
+    class Originator
+    {
+        public string State { get; set; }
+        public void SetMemento(Memento memento)
+        {
+            State = memento.State;
+        }
+        public Memento CreateMemento()
+        {
+            return new Memento(State);
+        }
+    }
+}
